135329b6419110fb03b60007508782cb9e18ffe6
Alexey Apalenov <aapalenov@artel-workshop.com>
2023-02-06 21:13:31 +0700

    add search bar

--- components/Layout/SearchBar.vue
+++ components/Layout/SearchBar.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="search-bar">
-    <div class="icon-button">
-      <search-icon class="search-icon" />
-    </div>
+    <input
+        v-show="isExpanded"
+        class="search-bar-input"
+        :class="{ 'search-bar-expanded': isExpanded }"
+        type="text"
+    />
+      <div
+          class="search-bar-icon"
+          :class="{'search-bar-icon-expanded' : isExpanded}"
+          @click="toggleExpanded"
+      >
+        <search-icon class="search-icon" />
+      </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import SearchIcon from '~/assets/svg/search.svg'
+
+const isExpanded = ref(false)
+
+const toggleExpanded = () => {
+  isExpanded.value = !isExpanded.value
+}
 </script>
