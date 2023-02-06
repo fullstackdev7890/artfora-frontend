f7cbd8172fc04390a02d3ed496a1f48d8638bc38
Alexey Apalenov <aapalenov@artel-workshop.com>
2023-02-06 10:59:09 +0700

    hide/show header

--- components/Layout/TheHeader.vue
+++ components/Layout/TheHeader.vue
@@ -1,5 +1,8 @@
 <template>
-  <header class="header">
+  <header
+    class="header"
+    :class="{ 'header-hide': hideHeader }"
+  >
     <div class="header-top">
       <search-bar />
 
@@ -15,6 +18,32 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
 import SearchBar from '~/components/Layout/SearchBar.vue'
 import ProfileMenu from '~/components/Layout/ProfileMenu.vue'
+
+const hideHeader = ref(false)
+const scrollPosition = ref(0)
+
+onMounted(() => {
+  if (process.client) {
+    scrollPosition.value = window.scrollY
+    window.addEventListener('scroll', handleScroll);
+    handleScroll()
+  }
+})
+
+onUnmounted(() => {
+  if (process.client) {
+    scrollPosition.value = window.scrollY
+    window.removeEventListener('scroll', handleScroll);
+  }
+})
+
+function handleScroll () {
+  let currentScrollPosition = window.scrollY
+  hideHeader.value = currentScrollPosition > scrollPosition.value && currentScrollPosition > 100
+  scrollPosition.value = window.scrollY
+}
+
 </script>
