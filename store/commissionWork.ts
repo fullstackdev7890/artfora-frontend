30e2c82d058a6a7cfa8a0248eda2ee4d0b1ea210
Roman Dubrovin <rdubrovin@artel-workshop.com>
2023-02-12 23:41:13 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- store/commissionWork.ts
+++ store/commissionWork.ts
@@ -2,7 +2,6 @@ import { defineStore } from 'pinia'
 import { CommissionWork } from '~/types/commissionWork'
 import axios from 'axios'
 
-
 export const useCommissionWorkState = defineStore('commission-work', {
   actions: {
     async send(userId: number, data: CommissionWork) {
