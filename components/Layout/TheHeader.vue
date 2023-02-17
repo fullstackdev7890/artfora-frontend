f79611bf771dbb4435d5a57a91be601623c878a2
Alexey Apalenov <aapalenov@artel-workshop.com>
2023-02-17 22:49:10 +0600

    #-: removed listener for header.

--- components/Layout/TheHeader.vue
+++ components/Layout/TheHeader.vue
@@ -1,8 +1,5 @@
 <template>
-  <header
-    :class="{ 'header-hide': isShown }"
-    class="header"
-  >
+  <header class="header">
     <div class="header-top">
       <search-bar />
 
@@ -20,34 +17,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue'
-import { HEADER_HEIGHT } from '~/types'
 import SearchBar from '~/components/Layout/SearchBar.vue'
 import ProfileMenu from '~/components/Layout/ProfileMenu.vue'
 import Categories from '~/components/Layout/Categories.vue'
-
-const isShown = ref(false)
-const scrollPosition = ref(0)
-
-onMounted(() => {
-  if (process.client) {
-    scrollPosition.value = window.scrollY
-    window.addEventListener('scroll', handleScroll);
-    handleScroll()
-  }
-})
-
-onUnmounted(() => {
-  if (process.client) {
-    scrollPosition.value = window.scrollY
-    window.removeEventListener('scroll', handleScroll);
-  }
-})
-
-function handleScroll () {
-  let currentScrollPosition = window.scrollY
-  isShown.value = currentScrollPosition > scrollPosition.value && currentScrollPosition > HEADER_HEIGHT
-  scrollPosition.value = window.scrollY
-}
-
 </script>
