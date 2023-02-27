f437784d608fbb71ffb3d91d9f27a56f5f1c22ca
Alexey Apalenov <aapalenov@artel-workshop.com>
2023-02-27 17:12:22 +0700

    implement search flow

--- components/Layout/SearchBar.vue
+++ components/Layout/SearchBar.vue
@@ -6,16 +6,18 @@
     >
       <input
         v-show="isExpanded"
+        v-model="search"
+        @input="findProducts"
         class="search-bar-content-input"
         type="text"
       />
-        <div
-          :class="{'search-bar-content-icon-expanded' : isExpanded}"
-          @click="toggleExpanded"
-          class="search-bar-content-icon"
-        >
-          <search-icon class="search-icon" />
-        </div>
+      <div
+        :class="{'search-bar-content-icon-expanded' : isExpanded}"
+        @click="toggleExpanded"
+        class="search-bar-content-icon"
+      >
+        <search-icon class="search-icon" />
+      </div>
     </div>
   </div>
 </template>
@@ -23,8 +25,42 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import SearchIcon from '~/assets/svg/search.svg'
+import { useProductsStore } from '~/store/products'
 
 const isExpanded = ref(false)
+const products = useProductsStore()
+const search = ref('')
+let timer: ReturnType<typeof setTimeout> | null = null
 
-const toggleExpanded = () => isExpanded.value = !isExpanded.value
+const findProducts = () => {
+
+  if (timer) {
+    clearTimeout(timer)
+  }
+
+  timer = setTimeout(() => {
+
+    if (search.value.length < 3) {
+      products.updateFilter({ query: null })
+      products.fetchAll()
+      return
+    }
+
+    products.updateFilter({ query: search.value })
+    products.fetchAll()
+
+  }, 1000)
+}
+
+const toggleExpanded = () => {
+  isExpanded.value = !isExpanded.value
+
+  if (!isExpanded.value) {
+    search.value = ''
+    products.updateFilter({ query: null })
+    products.fetchAll()
+  }
+}
+
 </script>
+
