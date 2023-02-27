eede511fca54bba5e2bd0a5c5a7975fdba93cc3d
Alexey Apalenov <aapalenov@artel-workshop.com>
2023-02-27 19:20:47 +0700

    fix layout

--- components/Modals/AiSafeDescriptionModal.vue
+++ components/Modals/AiSafeDescriptionModal.vue
@@ -6,17 +6,17 @@
   >
     <template v-slot:content>
       <div class="ai-safe-description">
-        <p>In an attempt to hide your images from server <br>
+        <p>In an attempt to hide your images from server
           crawling bots, we will:</p>
         <ul>
           <li>Add rubbish tags.</li>
           <li>Add copyright.</li>
           <li>Add invisible watermark.</li>
         </ul>
-        <p>Which hopefully decrease the likelyhood that your <br>
+        <p>Which hopefully decrease the likelyhood that your
           images will be found and (mis)used.</p>
-        <p>The downside is that your images can't be used <br>
-          when we promote ARTfora and you may miss <br>
+        <p>The downside is that your images can't be used
+          when we promote ARTfora and you may miss
           an opportunity to get your images promoted.</p>
         <a
           href="https://en.wikipedia.org/wiki/Artificial_intelligence_art"
