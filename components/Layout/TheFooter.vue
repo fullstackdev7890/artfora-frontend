7981095b07cefff2422ccb3dcb01f39d178e66c3
vkokhan <kokhanvolodymyr21@gmail.com>
2023-06-21 14:51:45 +0300

    change text on footer

--- components/Layout/TheFooter.vue
+++ components/Layout/TheFooter.vue
@@ -3,7 +3,7 @@
 
     <div class="footer-bottom">
       <div class="footer-bottom-kofi">
-        <span>Help keeping this gallery free for artists: </span>
+        <span>Help me keep this gallery free for artists: </span>
         <a href='https://ko-fi.com/G2G0M7GR0' target='_blank'><img height='40' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
       </div>
     </div>
