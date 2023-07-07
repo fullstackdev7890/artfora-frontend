249334f3b606e8f946283102c7349efa6a955f9d
vkokhan <kvolodymyr.dev@gmail.com>
2023-07-07 11:50:47 +0300

    about and faq modal text

--- components/Modals/AboutArtforaModal.vue
+++ components/Modals/AboutArtforaModal.vue
@@ -1,42 +1,49 @@
 <template>
-    <ui-kit-modal
-      :title="'About ARTfora'"
-      :with-footer="false"
-      class="artfora-modal"
-      ref="aboutArtforaForm"
-    >
-      <template v-slot:content>
-          <div  v-for="(artfora, index) in artforas" :key="index">
-            <div class="artfora-title" :style="{color:artfora.text_colour, paddingBottom:artfora.text_type==='divide'?'2.5rem':'0.5rem'}">{{artfora.text_content}} </div>
+  <ui-kit-modal
+    :title="'About ARTfora'"
+    :with-footer="false"
+    class="artfora-modal"
+    ref="aboutArtforaForm"
+  >
+    <template v-slot:content>
+      <div v-for="(artfora, index) in artforas" :key="index">
+        <div
+          class="artfora-title"
+          :style="{
+            color: artfora.text_colour,
+            paddingBottom: artfora.text_type === 'divide' ? '2.5rem' : '0.5rem',
+          }"
+          v-html="artfora.text_content"
+        ></div>
+      </div>
+      <div class="ui-kit-modal-content-buttons">
+        <button class="button button-grey full-width" @click="close">
+          <span>Close</span>
+        </button>
+      </div>
+    </template>
+  </ui-kit-modal>
+</template>
 
-          </div>
-        <div class="ui-kit-modal-content-buttons">
-          <button
-            class="button button-grey full-width"
-            @click="close"
-          ><span>Close</span></button>
-        </div>
-      </template>
-    </ui-kit-modal>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from '@vue/reactivity'
-  import UiKitModal from '~/components/UiKit/UiKitModal.vue'
-  import { useTextsStore } from '~/store/texts'
-  
-  const aboutArtforaForm = ref<InstanceType<typeof UiKitModal>>(null)
-  const textsStore = useTextsStore()
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import UiKitModal from "~/components/UiKit/UiKitModal.vue";
+import { useTextsStore } from "~/store/texts";
 
-    const artforas = computed(() => textsStore?.getArtfora())
-    const artforaheaderColor = computed(() => artforas?.value ? artforas.value[0]?.text_colour : '')
-  function open() {
-    aboutArtforaForm.value?.open()
-  }
-  
-  function close() {
-    aboutArtforaForm.value?.close()
-  }
-  
-  defineExpose({ open, close })
-  </script>
+const aboutArtforaForm = ref<InstanceType<typeof UiKitModal>>(null);
+const textsStore = useTextsStore();
+
+const artforas = computed(() => textsStore?.getArtfora());
+const artforaheaderColor = computed(() =>
+  artforas?.value ? artforas.value[0]?.text_colour : ""
+);
+function open() {
+  aboutArtforaForm.value?.open();
+}
+
+function close() {
+  aboutArtforaForm.value?.close();
+}
+
+defineExpose({ open, close });
+</script>
--- components/Modals/FAQModal.vue
+++ components/Modals/FAQModal.vue
@@ -1,43 +1,41 @@
 <template>
-  <ui-kit-modal
-    :title="'FAQ'"
-    :with-footer="false"
-    class="faq-modal"
-    ref="faqModalForm"
-  >
-
+  <ui-kit-modal :title="'FAQ'" :with-footer="false" class="faq-modal" ref="faqModalForm">
     <template v-slot:content>
-      <div  v-for="(faq, index) in faqs" :key="index">
-          <div class="faq-title" :style="{color:faq.text_colour, paddingBottom:faq.text_type==='divide'?'2.5rem':'0.5rem'}">{{faq.text_content}} </div>
-        
+      <div v-for="(faq, index) in faqs" :key="index">
+        <div
+          class="faq-title"
+          :style="{
+            color: faq.text_colour,
+            paddingBottom: faq.text_type === 'divide' ? '2.5rem' : '0.5rem',
+          }"
+          v-html="faq.text_content"
+        ></div>
       </div>
       <div class="ui-kit-modal-content-buttons">
-        <button
-          class="button button-grey full-width"
-          @click="close"
-        ><span>Close</span></button>
+        <button class="button button-grey full-width" @click="close">
+          <span>Close</span>
+        </button>
       </div>
     </template>
   </ui-kit-modal>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { computed } from 'vue'
-import UiKitModal from '~/components/UiKit/UiKitModal.vue'
-import { useTextsStore } from '~/store/texts'
+import { ref } from "vue";
+import { computed } from "vue";
+import UiKitModal from "~/components/UiKit/UiKitModal.vue";
+import { useTextsStore } from "~/store/texts";
 
-
-const textsStore = useTextsStore()
-const faqModalForm = ref<InstanceType<typeof UiKitModal>>(null)
-  const faqs = computed(() => textsStore?.getFaq())
-  function open() {
-    faqModalForm.value?.open()
-  }
-  
-function close() {
-  faqModalForm.value?.close()
+const textsStore = useTextsStore();
+const faqModalForm = ref<InstanceType<typeof UiKitModal>>(null);
+const faqs = computed(() => textsStore?.getFaq());
+function open() {
+  faqModalForm.value?.open();
 }
 
-defineExpose({ open, close })
+function close() {
+  faqModalForm.value?.close();
+}
+
+defineExpose({ open, close });
 </script>
